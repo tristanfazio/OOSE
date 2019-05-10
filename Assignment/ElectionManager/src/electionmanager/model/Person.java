00023ddb400de526d227e16166e03d609dc7d865
tristanfazio <2tfazio@iinet.net.au>
2019-05-10 18:12:56 +0800

    person class

---
+++ Assignment/ElectionManager/src/electionmanager/model/Person.java
@@ -0,0 +1,28 @@
+package electionmanager.model;
+
+import java.util.*;
+
+/******
+Person Class
+Represents a person object
+
+@author Tristan Fazio
+Object Oriented Software Engineering
+Semester One, 2019
+Curtin University
+******/
+
+public class Person
+{
+    //CLASSFIELDS
+    private int idNum;
+    private String name;
+    private String type;
+
+    //GETTERS
+
+    //SETTERS
+
+    //METHODS
+
+}
