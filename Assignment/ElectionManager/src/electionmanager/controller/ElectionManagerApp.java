06c8cbe61b0ed4f4be87c0c836c6e04f9b3475a8
tristanfazio <2tfazio@iinet.net.au>
2019-05-11 17:50:10 +0800

    setting up person class, setting up ant build

---
+++ Assignment/ElectionManager/src/electionmanager/controller/ElectionManagerApp.java
@@ -0,0 +1,20 @@
+package electionmanager.controller;
+
+import java.util.*;
+
+/*****
+* Main app class for Election Manager
+* 
+* @author Tristan Fazio
+*Object Oriented Software Engineering
+*Semester One, 2019
+*Curtin University
+******/
+
+public class ElectionManagerApp
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
--- Assignment/ElectionManager/src/electionmanager/model/Person.java
+++ Assignment/ElectionManager/src/electionmanager/model/Person.java
@@ -3,26 +3,54 @@ package electionmanager.model;
 import java.util.*;
 
 /******
-Person Class
-Represents a person object
-
-@author Tristan Fazio
-Object Oriented Software Engineering
-Semester One, 2019
-Curtin University
+*Person Class
+*Represents a person object
+*
+*@author Tristan Fazio
+*Object Oriented Software Engineering
+*Semester One, 2019
+*Curtin University
 ******/
 
-public class Person
+public abstract class Person
 {
     //CLASSFIELDS
     private int idNum;
     private String name;
     private String type;
+    private Set<Contact> contacts;
+    //CONSTRUCTOR
+    public Person()
+    {
 
+    }
     //GETTERS
-
+    public int getID(){return idNum;}
+    public String getName(){return name;}
+    public String getType(){return type;}
+    public Set<Contact> getContacts(){return contacts;}
     //SETTERS
+    public void setID(int inID)
+    {
+        this.idNum = inID;
+    }
 
+    public void setName(String inName)
+    {
+        this.name = inName;
+    }
+
+    abstract void setType();
+
+    public void addContact(Contact inContact)
+    {
+        contacts.add(inContact);
+    }
+    
+    public void removeContact(Contact inContact)
+    {
+        contacts.remove(inContact);
+    }
     //METHODS
 
 }
